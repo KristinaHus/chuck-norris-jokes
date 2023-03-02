e3724ad58e457759ca65233f16a9b689e525806f
KristinaHus <kina.girl@gmail.com>
2023-03-02 21:24:21 +0100

    Test pre-commit hook

--- src/components/Link.js
+++ src/components/Link.js
@@ -8,11 +8,9 @@ const STATUS = {
 
 export default function Link({ page, children }) {
 	const [status, setStatus] = useState(STATUS.NORMAL);
-
 	const onMouseEnter = () => {
 		setStatus(STATUS.HOVERED);
 	};
-
 	const onMouseLeave = () => {
 		setStatus(STATUS.NORMAL);
 	};
