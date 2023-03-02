b202713bdd32b141fc901ac98020d3205824405b
KristinaHus <kina.girl@gmail.com>
2023-03-02 21:14:52 +0100

    test pre-commit hook

--- src/tests/components/Link.test.js
+++ src/tests/components/Link.test.js
@@ -2,7 +2,7 @@ import renderer from 'react-test-renderer';
 import Link from '../../components/Link';
 
 it('changes the class when hovered', () => {
-	const component = renderer.create(<Link page="http://www.instagram.com">Facebook</Link>);
+	const component = renderer.create(<Link page="http://www.google.com">Facebook</Link>);
 	let tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
 
