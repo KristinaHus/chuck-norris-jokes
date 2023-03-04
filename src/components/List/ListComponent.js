72b90521779a3121b2b5ad0f54041c013a3f49bb
KristinaHus <kina.girl@gmail.com>
2023-03-04 11:47:45 +0100

    Remove mocked data from List

--- src/components/List/ListComponent.js
+++ src/components/List/ListComponent.js
@@ -3,26 +3,11 @@ import PropTypes from 'prop-types';
 
 import { ListItemComponent } from './ListItemComponent';
 
-export const ListComponent = ({ data, onPress }) => {
-	const listData = data || [
-		{ id: 'one', value: 'one' },
-		{ id: 'two', value: 'two' },
-		{ id: 'three', value: 'three' },
-		{ id: 'four', value: 'four' },
-		{ id: 'five', value: 'five' },
-		{ id: 'six', value: 'six' },
-		{ id: 'seven', value: 'seven' },
-		{ id: 'eight', value: 'eight' },
-		{ id: 'nine', value: 'nine' },
-		{ id: 'ten', value: 'ten' },
-		{ id: 'eleven', value: 'eleven' },
-		{ id: 'twelve', value: 'twelve' }
-	];
-
+export const ListComponent = ({ data = [], onPress }) => {
 	return (
 		<div>
 			<List sx={{ width: '100%', bgcolor: 'background.paper' }} aria-label="contacts">
-				{listData.map((item) => (
+				{data.map((item) => (
 					<ListItemComponent item={item} key={item.id} onPress={onPress} />
 				))}
 			</List>
